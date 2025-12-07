7e848ab96bbcd5900bebb7f1c773386156008fed
vladteyhd <vladteyhd@mail.ru>
2025-12-07 13:54:56 +0300

    first

---
+++ newyear.js
@@ -0,0 +1,98 @@
+import {mlog} from './vendor/logs.mjs'
+process.on('uncaughtException', (err) => {
+mlog('Глобальный косяк приложения!!! ', err.stack);
+}); //Если все пошло по ***, спасет ситуацию
+
+import express from 'express'
+import exphbs from 'express-handlebars'
+import session from 'express-session'
+import cookieParser from 'cookie-parser'
+import path from 'path'
+import fs from 'fs-extra'
+import { fileURLToPath } from 'url';
+
+var PORT = process.env.PORT || 91;
+
+const app = express();
+const hbs = exphbs.create({
+defaultLayout: 'main',
+extname: 'hbs',
+helpers: {
+    OK: function(){
+    i_count = 1
+    },
+    I_C: function (opts){
+    let anso = ''
+    for (let i = 0; i < i_count; i++) {
+        anso = anso + "I"
+    }
+    i_count++
+    return anso
+    },
+    PLS: function (a,opts){
+
+        return a+10
+        },
+    if_eq: function (a, b, opts) {
+        if (a == b){ // Or === depending on your needs
+            // logman.log(opts);
+            return opts.fn(this);
+        } else
+            return opts.inverse(this);
+    },
+    if_more: function (a, b, opts) {
+    if (a >= b){ // Or === depending on your needs
+        // logman.log(opts);
+        return opts.fn(this);
+        } else
+        return opts.inverse(this);
+    },
+    for: function(from, to, incr, block) {
+        var accum = '';
+        for(var i = from; i < to; i += incr)
+            accum += block.fn(i);
+        return accum;
+    }
+}
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export let appDir = __dirname;
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+const viewsPath = path.join(appDir, 'views');
+const publicPath = path.join(appDir, 'public');
+
+app.set('views', viewsPath);
+mlog(publicPath);
+app.use(express.static(publicPath));
+
+app.use(cookieParser());
+app.set('trust proxy', 1);
+
+app.use(session({name: 'sso.sid',resave:true,saveUninitialized:false, secret: 'hardcode_secret_teyhd', cookie: 
+  {secure: false, // ⚠️ обязательно false на HTTP!
+  httpOnly: true}
+}))
+
+app.get('/',async (req,res)=>{
+    res.render('index',{
+      title: 'Новый год'
+    });
+  })
+
+async function start(){
+    try {
+        app.listen(PORT,()=> {
+            mlog('Новый год - запущен')
+           // say('Распределительный портал - запущен \nПорт: '+PORT)
+            mlog('Порт:',PORT);
+        })
+    } catch (e) {
+        mlog(e);
+    }
+}
+start();
---
+++ vendor/logs.mjs
@@ -0,0 +1,62 @@
+import fs from 'fs-extra'
+import path from 'path'
+
+import request from 'request'
+import urlencode from 'urlencode'
+
+import { fileURLToPath } from 'url';
+
+// Получаем __dirname в ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export let appDir = __dirname;
+
+function curdate(minute){
+    minute = (minute < 10) ? '0' + minute : minute;
+    return minute;
+  }
+  
+export function mlog (par) {
+    let datecreate = new Date();
+    let texta = `\n ${curdate(datecreate.getHours())}:${curdate(datecreate.getMinutes())}:${curdate(datecreate.getSeconds())}`;
+    let obj = arguments;
+  
+    for (const key in obj) {
+      if (typeof obj[key]=='object') {
+        for (const keys in obj[key]){
+          texta = `${texta} \n ${keys}:${obj[key][keys]}`
+        }
+      } else {
+        texta = `${texta} ${obj[key]}`
+      }
+      
+    } 
+    fs.writeFileSync(path.join(appDir,'logs',`${curdate(datecreate.getDate())}.${curdate(datecreate.getMonth()+1)} log.txt`),
+    texta,
+    {
+      encoding: "utf8",
+      flag: "a+",
+      //mode: 0o666
+    });
+  
+    console.log(texta);
+    return texta
+  }
+
+export function say(msg,all=true) {
+  var numb = ['79176334420']
+  var tgnum = [304622290,5662630619]
+  if (all===true){
+    tgnum.forEach(element => {
+      setTimeout(() => sendtg(element,msg), 1500);
+    });
+  } else{
+    setTimeout(() => sendtg(tgnum[0],msg), 1500);
+  }
+  
+}
+
+function sendtg(num,msg) {
+    request(`http://home.teyhd.ru:3334/?msg=${urlencode(msg)}&num=${urlencode(num)}`)
+}
+
